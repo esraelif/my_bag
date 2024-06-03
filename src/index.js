fe61d705c9150c9704251029a4a890426d810224
esraelif <elifesratunca@gmail.com>
2024-06-03 16:51:02 +0200

    "Updated README.md and src/index.js files"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
 
+// Create a root.
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(
+// Render the application.
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
+  </React.StrictMode>
+);
 
-  </React.StrictMode>,
-  document.getElementById('root')
-)
