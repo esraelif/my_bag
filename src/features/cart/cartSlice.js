4f2536916d9c6dd4da6fd9683a1a1d4cb8f9fa46
esraelif <elifesratunca@gmail.com>
2024-06-03 15:44:09 +0200

    "Added Redux actions and dispatchers to CartContainer and CartItem components, and created reducers for cart slice"

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -14,7 +14,27 @@ const initialState = {
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
+    reducers: {
+        clearCart: (state) => {
+            state.cartItems = []
+
+        },
+    },
+    removeItem: (state, action) => {
+        const itemId = action.payload;
+        state.cartItems = state.cartItems.filter((item) => item.id !== itemId)
+    },
+    increase: (state, { payload }) => {
+
+        const cartItem = state.cartItems.find((item) => item.id === payload.id)
+        cartItem.amount = cartItem.amount + 1
+    },
+    decrease: (state, { payload }) => {
+
+        const cartItem = state.cartItems.find((item) => item.id === payload.id)
+        cartItem.amount = cartItem.amount - 1
+    }
 })
 export default cartSlice.reducer;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions
 
-// console.log(cartSlice)
